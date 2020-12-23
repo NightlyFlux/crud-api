360507fe473b14cb28a5233ef8138960a0ba87f0
NightlyFlux <nightlyflux@gmail.com>
2020-12-22 22:08:39 -0800

    Initial Commit

---
+++ start/routes.js
@@ -0,0 +1,10 @@
+'use strict'
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.group(() => {
+  Route.get('/employees', 'EmployeeController.index')
+  Route.post('/employees', 'EmployeeController.store')
+  Route.patch('/employees/:id', 'EmployeeController.update')
+  Route.delete('/employees/:id', 'EmployeeController.destroy')
+}).prefix('api/v1')
