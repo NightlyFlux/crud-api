27062b797d764e904fae2aec3805e070d8d42148
NightlyFlux <nightlyflux@gmail.com>
2020-12-22 22:48:43 -0800

    Done.

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: 'https://c-crud-app.netlify.app/',
+  origin: '*',
 
   /*
   |--------------------------------------------------------------------------
