f29b20b4f6c34b852541b093b6f138a7dc91847a
Salvador Gómez <nightlyflux@gmail.com>
2022-10-30 20:53:06 -0700

    refactor: update adonis to latest version

---
+++ app/Controllers/Http/EmployeesController.ts
@@ -0,0 +1,70 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Employee from 'App/Models/Employee'
+
+export default class EmployeesController {
+  public async index({ response }: HttpContextContract) {
+    const employees = await Employee.query()
+      .select(['employees.id', 'name', 'email', 'address', 'phone', 'role', 'role_id'])
+      .innerJoin('roles', 'employees.role_id', 'roles.id')
+
+    if (employees) {
+      return response.json(employees)
+    } else {
+      return response.status(500).json({ message: 'There was an error' })
+    }
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const { name, email, address, phone, role_id } = request.body() as unknown as Employee
+
+    await Employee.create({
+      name,
+      email,
+      address,
+      phone,
+      role_id,
+    }).then(
+      (newEmployee) => response.json(newEmployee),
+      () => response.status(500).json({ message: 'There was an error' })
+    )
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const { name, email, address, phone, role_id } = request.body() as unknown as Employee
+    const updatedEmployee = await Employee.find(params.id)
+
+    if (updatedEmployee) {
+      await updatedEmployee
+        .merge({
+          name,
+          email,
+          address,
+          phone,
+          role_id,
+        })
+        .save()
+        .then(
+          () =>
+            response.json({
+              message: `Employee with ID: ${params.id} was updated successfully`,
+            }),
+          () => response.status(500).json({ message: 'There was an error' })
+        )
+    }
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const employee = await Employee.findOrFail(params.id)
+    if (employee) {
+      await employee.delete().then(
+        () =>
+          response.json({
+            message: `Employee with ID: ${params.id} was deleted successfully`,
+          }),
+        () => response.status(500).json({ message: 'There was an error' })
+      )
+    }
+  }
+}
---
+++ database/migrations/1667115584956_employees.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'employees'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.string('email').notNullable()
+      table.string('address').notNullable()
+      table.specificType('phone', 'bigint(10)').notNullable()
+      table.integer('role_id', 10).unsigned().references('roles.id')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
